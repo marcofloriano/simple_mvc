800d25fe66d9e69aca808b8c0d6ea1cb1b2b0eed
marcofloriano <marcofloriano@gmail.com>
2021-09-08 12:18:35 -0300

    Index atualizado

--- Application/views/home/index.php
+++ Application/views/home/index.php
@@ -1,7 +1,26 @@
 <div class="container">
   <div class="row">
     <div class="col-8 offset-2 text-center" style="margin-top:150px">
-      <h1>Construindo um simples Framework MVC com PHP</h1>
+      <h1>Painel de Controle</h1>
+      <br>
+      <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Opção</th>
+              <th scope="col">Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Usuários</td>
+              <td><a href="/user">Gerenciar</a> | <a href="/user/create">Adicionar</a></td>
+            </tr>
+            <tr>
+              <td>Posts</td>
+              <td><a href="/post">Gerenciar</a> | <a href="/post/create">Adicionar</a></td>
+            </tr>
+          </tbody>
+        </table>
     </div>
   </div>
 </div>
