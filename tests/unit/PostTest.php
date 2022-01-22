c7c7c6f09061cc9f569488cd19abd36bfa5cecea
marcofloriano <marcofloriano@gmail.com>
2022-01-22 14:22:30 -0300

    Refactoring testes

--- tests/unit/PostTest.php
+++ tests/unit/PostTest.php
@@ -2,10 +2,16 @@
 
 class PostTest extends \PHPUnit\Framework\TestCase
 {
-	public function testThatWeCanFindAllPosts()
+	protected $post;
+
+	public function setUp() :void
 	{
-		$post = new \Application\models\Posts;
-		$posts = $post->findAll();
+		$this->post = new \Application\models\Posts;
+	}
+
+	public function testThatWeCanFindAllPosts()
+	{		
+		$posts = $this->post->findAll();
 		foreach ($posts as $post) {
 			$this->assertArrayHasKey( 'id' ,$post);
 		}
@@ -13,41 +19,34 @@ class PostTest extends \PHPUnit\Framework\TestCase
 
 	public function testThatWeCanFindPostById()
 	{
-		$post = new \Application\models\Posts;
-		$posts = $post->findAll();
+		$posts = $this->post->findAll();
 		$id = $posts[0]['id'];
-		$newPost = $post->findById($id);
+		$newPost = $this->post->findById($id);
 		$this->assertArrayHasKey( 'id' , $newPost[0]);
 	}
 
 	public function testThatWeCanInsertPost()
 	{
-		$post = new \Application\models\Posts;
 		$newPost = array();
 		$newPost['titulo'] = 'Santo do Dia 19 de Janeiro';
     	$newPost['texto'] = 'Em Esmirna, hoje Izmir, na Turquia, a paixão de São Germânico, mártir de Filadélfia, que, no tempo dos imperadores Marco Antonino e Lúcio Aurélio, foi discípulo de São Policarpo, a quem precedeu no martírio: condenado pelo juiz ainda na flor da idade juvenil, superou pela graça de Deus o medo da sua fragilidade corporal e provocou espontaneamente a fera para ele preparada.';
-
-    	$this->assertTrue($post->insertPost($newPost));
+    	$this->assertTrue($this->post->insertPost($newPost));
 	}
 
 	public function testThatWeCanUpdatePost()
 	{
-		$post = new \Application\models\Posts;
-		$posts = $post->findAll();
+		$posts = $this->post->findAll();
 		$lastPost = end($posts);
 		$lastPost['titulo'] = 'Santo do Dia 19 de Janeiro (2)';
 		$lastPost['texto'] = 'Em Spoleto, na Úmbria, região da Itália, São Ponciano, mártir, que, no tempo do imperador Antonino, crudelissimamente flagelado por amor de Cristo, foi finalmente morto ao fio da espada.';
-
-		$this->assertTrue($post->updatePost($lastPost));
+		$this->assertTrue($this->post->updatePost($lastPost));
 	}
 
 	public function testThatWeCanDeletePost()
 	{
-		$post = new \Application\models\Posts;
-		$posts = $post->findAll();
+		$posts = $this->post->findAll();
 		$lastPost = end($posts);
 		$id = $lastPost['id'];
-
-		$this->assertTrue($post->deleteById($id));
+		$this->assertTrue($this->post->deleteById($id));
 	}
 }
--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -3,79 +3,72 @@ session_start();
 
 class UserTest extends \PHPUnit\Framework\TestCase
 {
+	protected $user;
+
+	public function setUp() :void 
+	{
+		$this->user = new \Application\models\Users;
+	}
+
 	public function testThatWeCanFindAllUsers()
 	{
-		$user = new \Application\models\Users;
-		$users = $user->findAll();
-		foreach( $users as $user ) {
-			$this->assertArrayHasKey( 'id' ,$user);
+		$usuarios = $this->user->findAll();
+		foreach( $usuarios as $usuario ) {
+			$this->assertArrayHasKey( 'id' ,$usuario);
 		}
-
 	}
 
 	public function testThatWeCanFindUserById()
 	{
-		$user = new \Application\models\Users;
-		$users = $user->findAll();
-		$id = $users[0]['id'];
-		$newUser = $user->findById($id);
+		$usuarios = $this->user->findAll();
+		$id = $usuarios[0]['id'];
+		$newUser = $this->user->findById($id);
 		$this->assertArrayHasKey( 'id' , $newUser[0]);
 	}
 
 	public function testThatWeCanInsertUser()
 	{
-		$user = new \Application\models\Users;
 		$usuario = array();
 		$usuario['nome'] = 'Marco Floriano Teste';
     	$usuario['usuario'] = 'marcofloriano_teste';
     	$usuario['senha'] = 'teste.123';
     	$usuario['email'] = 'marcofloriano@gmail.com';
-
-    	$this->assertTrue($user->insertUser($usuario));
+    	$this->assertTrue($this->user->insertUser($usuario));
 	}
 
 	public function testThatWeCanUpdateUser()
 	{
-		$user = new \Application\models\Users;
-		$users = $user->findAll();
-		$lastUser = end($users);
+		$usuarios = $this->user->findAll();
+		$lastUser = end($usuarios);
 		$lastUser['nome'] = 'M F Teste';
 		$lastUser['usuario'] = 'mfteste';
 		$lastUser['email'] = 'mfteste@setor9.com.br';
 		$lastUser['senha'] = 'mfteste.123@#';
-
-		$this->assertTrue($user->updateUser($lastUser));
+		$this->assertTrue($this->user->updateUser($lastUser));
 	}
 
 	public function testThatWeCanResetUserPassword()
 	{
-		$user = new \Application\models\Users;
-		$users = $user->findAll();
-		$lastUser = end($users);
-
-		$this->assertTrue($user->resetPassword($lastUser['id'], 'floriano.123'));
+		$usuarios = $this->user->findAll();
+		$lastUser = end($usuarios);
+		$this->assertTrue($this->user->resetPassword($lastUser['id'], 'floriano.123'));
 	}
 
 	public function testThatWeCanAuthenticateUser()
 	{
-		$user = new \Application\models\Users;
-		$users = $user->findAll();
-		$lastUser = end($users);
+		$usuarios = $this->user->findAll();
+		$lastUser = end($usuarios);
 		$lastUser['username'] = $lastUser['usuario'];
 		$lastUser['password'] = 'floriano.123';
-
-		$checkUser = $user->authenticateUser($lastUser);
-
+		$checkUser = $this->user->authenticateUser($lastUser);
 		$this->assertEquals($checkUser[0]['id'], $lastUser['id']);
 	}
 
 	public function testThatWeCanDeleteUser()
 	{
-		$user = new \Application\models\Users;
-		$users = $user->findAll();
-		$lastUser = end($users);
+		$usuarios = $this->user->findAll();
+		$lastUser = end($usuarios);
 		$id = $lastUser['id'];
-
-		$this->assertTrue($user->deleteById($id));
+		$this->assertTrue($this->user->deleteById($id));
 	}
 }
